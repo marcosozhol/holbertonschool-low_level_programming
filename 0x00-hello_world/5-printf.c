00be8beb00ec99f3fde164a78faf4ad97b07ff39
marcosozhol <3956@holbertonschool.com>
2021-09-30 11:52:22 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+** main -Esto es un ejercicio de C
+** Description: printf
+** Return: 0
+**/
+
+int main(void)
+{
+	        printf("with proper grammar, but the outcome is a piece of art,")\n;
+		return (0);
+}
