6bba0a1ecb07c716973e1cc0844c3157888465e4
marcosozhol <3956@holbertonschool.com>
2021-09-30 11:37:40 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main -Esto es un ejercicio de C
+* Description: puts
+* Return: 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
+
