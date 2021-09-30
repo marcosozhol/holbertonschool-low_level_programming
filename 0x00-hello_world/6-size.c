7bf9676dc1e7e269f578aaec0aee80463e50ddcb
marcosozhol <3956@holbertonschool.com>
2021-09-30 13:05:27 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Esto es un ejercicio de C
+ * Description: size
+ * Return:0
+ */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of char: %lu byte(s)\n", sizeof(int));
+	printf("Size of char: %lu byte(s)\n", sizeof(long int));
+	printf("Size of char: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of char: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
