2040d3ee1f4959743cdb005d3fb4e5da28763a39
marcosozhol <3956@holbertonschool.com>
2021-10-03 16:59:36 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+/**
+*main - lenguages exercises c
+*Return: 0
+*/
+
+int main(void)
+{
+int num;
+int abc;
+for (num = '0'; num <= '9'; num++)
+putchar (num);
+for (abc = 'a'; abc <= 'f'; abc++)
+putchar (abc);
+putchar ('\n');
+return (0);
+}
