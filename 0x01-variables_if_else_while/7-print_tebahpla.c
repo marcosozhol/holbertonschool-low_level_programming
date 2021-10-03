c8d94d4f5f5591a08da33322115b7856a2eca4f7
marcosozhol <3956@holbertonschool.com>
2021-10-03 16:53:26 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+/**
+*main - lenguages exercises c
+*Return: 0
+*/
+
+int main(void)
+{
+int abc;
+for (abc = 'z'; abc >= 'a'; abc--)
+putchar (abc);
+putchar ('\n');
+return (0);
+}
