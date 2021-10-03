933e83b960a1791a2c520e66596b306c943625ac
marcosozhol <3956@holbertonschool.com>
2021-10-03 08:09:51 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+/**
+*main - lenguages exercises c
+*Return: 0
+*/
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+putchar(num);
+putchar('\n');
+return (0);
+}
