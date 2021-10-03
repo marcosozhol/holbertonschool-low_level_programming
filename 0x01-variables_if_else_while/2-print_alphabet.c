3b18ead36a3f505327dfdd41e427e5ea53103f2f
marcosozhol <3956@holbertonschool.com>
2021-10-03 07:16:36 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+/**
+*main - lenguages exercises c
+*Return: 0
+*/
+
+int main(void)
+{
+int abc;
+for (abc = 'a'; abc <= 'z'; abc++)
+putchar (abc);
+putchar ('\n');
+return (0);
+}
