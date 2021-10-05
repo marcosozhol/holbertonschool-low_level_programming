26881f1a024f0ebaf2e092007a4c924ef58a9674
marcosozhol <3956@holbertonschool.com>
2021-10-05 10:14:32 -0700

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - cabezera
+*
+*Return: Always 0.
+*/
+
+void print_alphabet_x10(void)
+{
+int abc, cont;
+cont = 0;
+	while(cont < 10)
+{
+	for (abc = 'a'; abc <= 'z'; abc++)
+{
+		_putchar(abc);
+}
+		cont++;
+		_putchar('\n');
+}
+}
