3610eb5231d0fe8573e58fc80408c1d43ca8470b
marcosozhol <3956@holbertonschool.com>
2021-10-06 03:28:33 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*jack_bauer - cabezera.
+*hs - horas
+*min - minutos
+*/
+void jack_bauer(void)
+{
+	int hs, min;
+	for (hs = 0; hs <= 23; hs++)
+{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hs / 10) + '0');
+			_putchar((hs % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar ((min % 10) + '0');
+			_putchar('\n');
+		}
+}
+}
