14acf09b436e144a4b4206cd7dfde8cb2a9a3ac8
marcosozhol <3956@holbertonschool.com>
2021-10-06 13:04:36 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add - Agrega dos numeros enteros..
+*@a: primer numero
+*@b: segundo numero
+*Return: retorna el resultado de la suma de los numeros.
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}
