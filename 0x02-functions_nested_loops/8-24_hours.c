08c623d8a532816e3192d57aeed76845ec062af9
marcosozhol <3956@holbertonschool.com>
2021-10-06 03:31:14 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,16 @@
 void jack_bauer(void)
 {
 	int hs, min;
-	for (hs = 0; hs <= 23; hs++)
+for (hs = 0; hs <= 23; hs++)
 {
-		for (min = 0; min <= 59; min++)
-		{
-			_putchar((hs / 10) + '0');
-			_putchar((hs % 10) + '0');
-			_putchar(':');
-			_putchar((min / 10) + '0');
-			_putchar ((min % 10) + '0');
-			_putchar('\n');
-		}
+	for (min = 0; min <= 59; min++)
+	{
+		_putchar((hs / 10) + '0');
+		_putchar((hs % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar ((min % 10) + '0');
+		_putchar('\n');
+	}
 }
 }
