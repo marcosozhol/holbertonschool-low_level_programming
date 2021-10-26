d1da7d4c6022d4480b2412bd2222ede7c4c15b02
marcosozhol <3956@holbertonschool.com>
2021-10-26 09:34:25 -0700

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *create_array - function to create array with chars
+ *@size: variable
+ *@c: variable
+ *Return: NULL or cadena
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	/* retorna null si size = 0 ó si falla*/
+	/* crear array de caracteres*/
+	/* inicia en un caracter especifico */
+
+	char *cadena;
+	unsigned int i;
+
+	cadena = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		cadena[i] = c;
+	}
+return (cadena);
+}
