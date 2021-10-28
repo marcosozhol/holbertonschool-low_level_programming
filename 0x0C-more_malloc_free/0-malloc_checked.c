b4be41b8d958150c79da015b6423a74e4deff584
marcosozhol <3956@holbertonschool.com>
2021-10-28 10:27:28 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -12,12 +12,11 @@ void *malloc_checked(unsigned int b)
 	void *ptr;
 
 	ptr = malloc(sizeof(unsigned int) * b + 1);
+		return (ptr);
 
 	if (ptr == NULL)
 	{
 		exit(98); /*sale de programa con 98*/
 	}		/*si ponemos return, saca con 98 lo anterior*/
 
-
-return (ptr);
 }
