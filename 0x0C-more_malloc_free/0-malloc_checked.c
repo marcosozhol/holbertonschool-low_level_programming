57332e80c974054b1b25908001302e1ad6a3ea29
marcosozhol <3956@holbertonschool.com>
2021-10-28 10:20:40 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *malloc_checked - muestra la direccion de memoria
+ *@b: variable
+ *Return: retorna la direccion o el fallo
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(unsigned int) * b);
+
+	if (ptr == NULL)
+	{
+		exit(98); /*sale de programa con 98*/
+	}		/*si ponemos return, saca con 98 lo anterior*/
+
+
+return (ptr);
+}
