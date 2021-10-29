2c5b31095038606094760d9d0abcfd06637f45d3
marcosozhol <3956@holbertonschool.com>
2021-10-28 23:22:49 -0700

    Task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *array_range - contrae dos valores
+ *@min: valor minimo
+ *@max: valor maximo
+ *Return: puntero a nuevo lugar
+ *
+ */
+
+int *array_range(int min, int max)
+{
+	int *z;
+	int i, x;
+
+	if (min > max)
+		return (NULL);
+
+	for (x = 0; x < (max - min); x++)
+
+	z = malloc(sizeof(int) * (x + 1));
+	if (z == NULL)
+		return (NULL);
+
+	for (i = 0; i <= x; i++)
+		z[i] = min++;
+
+return (z);
+}
