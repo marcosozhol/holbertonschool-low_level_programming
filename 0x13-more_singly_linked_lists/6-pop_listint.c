c94a955156ff5be42927109b7b2376cfacc9ff0a
marcosozhol <3956@holbertonschool.com>
2021-11-16 14:39:25 -0800

    Task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *pop_listint - elimina el nodo principal de una lista enlazada
+ *@head: puntero a puntero de head
+ *Return: el valor de n | 0 si head es null
+ *
+ */
+int pop_listint(listint_t **head)
+{
+	int i; /*va a tomar el valor de n de head*/
+	listint_t *x;
+
+	if (head == NULL)
+		return (0);
+
+	i = (*head)->n;
+
+	x = *head;
+	*head = (*head)->next;
+
+	free(x);
+	return (i);
+}
