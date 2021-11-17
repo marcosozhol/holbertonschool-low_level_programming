855c5efd83a9c9593e111627c71bd2f3b5c85200
marcosozhol <3956@holbertonschool.com>
2021-11-16 17:20:18 -0800

    Task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *get_nodeint_at_index - muestra el nodo que está en una posicion index
+ *@head: puntero al nodo head
+ *@index: posicion donde se encuentra el nodo n
+ *Return: Nodo en posicion index | NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int x; /*Contador para recorrer las lista*/
+
+	if (head)
+	{
+		if (head == NULL)
+			return (NULL);
+
+	for (x = 0; x < index; x++)
+	{
+		head = head->next;
+	}
+	}
+return (head);
+}
