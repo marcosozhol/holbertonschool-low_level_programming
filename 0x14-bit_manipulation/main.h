b4ac6359be9459d4e558c0cba7f9d60c66a11dee
marcosozhol <3956@holbertonschool.com>
2021-11-18 15:47:03 -0800

    Actualizado al ejercicio 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,8 @@
 #ifndef BIT_MANIPULATION
 #define BIT_MANIPULATION
+#include <unistd.h>
 int _putchar (char);
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
 
 #endif
