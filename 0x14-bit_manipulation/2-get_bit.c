297e53be178b33247e7d5e351e53f33e5bf9dc1e
marcosozhol <3956@holbertonschool.com>
2021-11-18 16:10:47 -0800

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
+ *get_bit - Devuelve el valor de un bit en un indice dado
+ *@n: valor del bit
+ *@index: indice dado
+ *Return: valor del indice | -1 si error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
