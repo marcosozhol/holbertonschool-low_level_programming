703fad35050a7bfad8f29c25da375eea1e500601
marcosozhol <3956@holbertonschool.com>
2021-11-22 11:32:22 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		write(new_file, text_content, i);
+		write(STDOUT_FILENO, text_content, i);
 	}
 	close(new_file);
 	return (1);
