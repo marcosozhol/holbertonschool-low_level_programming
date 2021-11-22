49d07c36d45dd47fa5636ed0e654f3eecfe303f0
marcosozhol <3956@holbertonschool.com>
2021-11-22 10:41:33 -0800

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*create_file - crea un archivo nuevo y solicitudes en casos
+*@filename: nombre del archivo
+*@text_content: contenido del archivo
+*Return: 1 en caso de exito | -1 falla
+*
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int new_file, i = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	new_file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		if (new_file == -1)
+		{
+			return (-1);
+		}
+
+	while (text_content[i])
+	{
+		i++;
+	}
+
+	if (text_content == NULL)
+	{
+		close(new_file);
+		return (-1);
+	}
+	else
+	{
+		write(new_file, text_content, i);
+	}
+	close(new_file);
+	return (1);
+}
