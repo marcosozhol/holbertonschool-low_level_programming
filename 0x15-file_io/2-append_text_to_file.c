08f1579369cd5b12d94815c12c794393ca008bba
marcosozhol <3956@holbertonschool.com>
2021-11-22 17:12:00 -0800

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,15 +19,22 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
 	for (i = 0; text_content[i] != '\0'; i++)
 
 	new_file = open(filename, O_WRONLY | O_APPEND);
+
 	if (new_file == -1)
 	{
 		return (-1);
 	}
 
 	write(new_file, text_content, i);
+
 	close(new_file);
 	return (1);
 }
