6d16ec6d4a60a896203a98722bcb717048079edb
marcosozhol <3956@holbertonschool.com>
2021-11-24 04:27:41 -0800

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,57 +11,59 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, rd, wr1;
+	int file_from, file_to, c1, c2, rd, wr1;
 	char *buf[1024];
 
-	if (argc != 2)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	if (argv[1] == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file 3-cp\n");
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file 3-cp\n"), exit(98);
 	}
 	file_from = open(argv[1], O_RDONLY, 0644);
 		if (file_from == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file_to\n");
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to file_to\n"), exit(99);
 		}
 	rd = read(file_from, buf, 1024);
 		if (rd == -1)
 		{
 			return (-1);
 		}
-
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 		if (file_to == -1)
 		{
 			return (-1);
 		}
-
 	wr1 = write(file_to, buf, 1024);
 		if (wr1 == -1)
 		{
 			return (-1);
 		}
-	closefun(file_from);
-	closefun(file_to);
+	c1 = close(file_from);
+		if (c1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d", c1), exit(100); }
+	c2 = close(file_to);
+		if (c2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close id fd %d\n", c2); }
 return (0);
 }
 /**
  *closefun - funcion para cerrar los open de arriba
- *@arch: archivos
- */
-void closefun(int arch)
-{
-
-	close(arch);
-		if (close(arch) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd -1\n");
-			exit(100);
-		}
-}
+ *@arch: archivos|
+ *
+*void closefun(int arch)
+*{
+*
+*	close(arch);
+*		if (close(arch) == -1)
+*		{
+*			dprintf(STDERR_FILENO, "Error: Can't close fd -1\n");
+*			exit(100);
+*		}
+*}
+*/
