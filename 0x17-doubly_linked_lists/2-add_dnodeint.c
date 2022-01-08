7d1b1098ed7f597e975fc09bd9178fe7183f8c32
marcosozhol <3956@holbertonschool.com>
2022-01-08 13:50:21 -0800

    Task 2 -  adds a new node at the beginning of a list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ *add_dnodeint - agrega un nodo al principio de la lista
+ *@head: cabecera de la lista
+ *@n: dato que se incerta en el nuevo nodo
+ *Return: nuevo nodo
+ *
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *nuevo_nodo;
+
+	nuevo_nodo = malloc(sizeof(dlistint_t));
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	if (nuevo_nodo == NULL)
+	{
+		return (NULL);
+	}
+
+	nuevo_nodo->n = n;
+
+	nuevo_nodo->next = *head;
+	nuevo_nodo->prev = NULL;
+
+	*head = nuevo_nodo;
+
+return (nuevo_nodo);
+}
