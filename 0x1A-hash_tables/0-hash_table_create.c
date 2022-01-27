30473889f64d180703d582e0301f4410ac298488
Marcosozhol <3956@holbertonschool.com>
2022-01-27 11:46:37 -0800

    Task 0 - Create a new table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create - Create a new table
+ *@size: size new table
+ *Return: pointer to new table
+ *
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+
+	if (size != NULL)
+	{
+		new_table = malloc(sizeof(hash_table_t));
+			new_table->array = malloc(sizeof(hash_node_t) * size);
+			new_table->size = size;
+
+	return (new_table);
+	}
+	else
+	return (NULL);
+}
