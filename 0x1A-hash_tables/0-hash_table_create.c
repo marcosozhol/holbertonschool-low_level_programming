5ded15c124981aa63865fbd5b1c1e795682070e5
Marcosozhol <3956@holbertonschool.com>
2022-01-28 05:55:41 -0800

    Task 0 - Correccion de check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,16 +9,31 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
+	new_table = malloc(sizeof(hash_table_t));
+
+	if (new_table == NULL)
+	{
+		return (NULL);
+	}
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 	else
 	{
-		new_table = malloc(sizeof(hash_table_t));
 			new_table->array = malloc(sizeof(hash_node_t) * size);
 			new_table->size = size;
+	}
+
+	if (new_table->array == NULL)
+	{
+		return (NULL);
+	}
+	if (new_table->size == 0)
+	{
+		return (NULL);
+	}
 
 	return (new_table);
-	}
 }
