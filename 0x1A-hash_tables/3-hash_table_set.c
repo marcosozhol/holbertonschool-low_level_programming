78674d993e544a2b8ee6fa9578f8254a1708a9db
Marcosozhol <3956@holbertonschool.com>
2022-01-30 05:16:26 -0800

    Task 3 - adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,29 +1,34 @@
 #include "hash_tables.h"
-/*
+/**
  *hash_table_set - adds an element to the hash table
  *@ht: hash table you want to add or update the key/value to
  *@key: is the key
  *@value:  is the value associated with the key
  *Return: 1 | 0
- *
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *nodo;
+	unsigned long int index = 0;
+	unsigned long int size = 0;
+	/*unsigned long int cmp;*/
+	hash_node_t *nodo = malloc(sizeof(hash_node_t));
 
-	if (ht == NULL)
-	{
+	if (!nodo)
 		return (0);
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+	size = ht->size;
+	index = key_index((unsigned char *)key, size);
+	if (ht->array[index] && strcmp(key, ht->array[index]->key) == 0)
+	{
+		free(ht->array[index]->value);
+		ht->array[index]->value = strdup(value);
+		return (1);
 	}
 
-	nodo = malloc(sizeof(hash_node_t));
-	if (nodo == NULL)
-	{
-		return (0);
-	}
-
-	nodo->key = strdup(key);
 	nodo->value = strdup(value);
-
+	nodo->next = ht->array[index];
+	ht->array[index] = nodo;
 	return (1);
 }
