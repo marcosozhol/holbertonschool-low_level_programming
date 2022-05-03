bd4a933aa604fcb0f446ae7985e4d20d2c8024e2
Marcosozhol <3956@holbertonschool.com>
2022-05-03 07:45:49 -0700

    Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,4 @@
-import "search_algos.h"
+#include "search_algos.h"
 /**
  * linear_search - searches for a value in an array of integers
  * @array:  pointer to the first element of the array to search in
