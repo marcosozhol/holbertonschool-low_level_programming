7085d30c1e4f8c1741a579914424de209cc26a6d
Marcosozhol <3956@holbertonschool.com>
2022-05-04 14:40:14 -0700

    Correction error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,20 +8,20 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    if (!array || size == 0)
-    {
-        return (-1);
-    }
+	if (!array || size == 0)
+	{
+		return (-1);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        printf ("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-        {
-            return (i);
-        }
-    }
-    return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
